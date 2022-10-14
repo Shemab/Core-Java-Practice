974678e901094cf73f48395ca645b0b3ab1a2efe
Shemab Alam <115772394+Shemab@users.noreply.github.com>
2022-10-14 11:28:19 +0530

    Add files via upload

---
+++ 12-10-22/PrintData.java
@@ -0,0 +1,10 @@
+Class PrintData
+{
+public static void main(String args[])
+{
+ System.out.println("Name  " +args[0]" + "+args[1]);
+     System.out.println("Age " +args[2]);
+      System.out.println("Course " +args[3]);
+          System.out.println("Grade  " +args[4]);
+}
+}
